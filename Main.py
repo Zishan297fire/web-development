61733faff1f806e4d5d71094b49b37d5ca3c9dee
Gojo7A <ramziyashan@gmail.com>
2026-05-09 20:41:15 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+# printing a greeting message
+print("Hello,World")
+# printing my name
+print("My name is Zishan.")
+# printing a fun fact about me
+# printing Iclove to play cricket
+print("I love to play cricket.")
+# performing and printing a simple calculation
+print("the result of 3+5 is:",3+5)
