98807535329408c8ee6216a972621a679f17e003
Gojo7A <ramziyashan@gmail.com>
2026-05-18 20:18:50 +0530

    Add files via upload

---
+++ mine.py
@@ -0,0 +1,9 @@
+print("Enter marks obtain in 4 subject:")
+math = int(input("enter your maths marks"))
+english = int(input("enter your english marks"))
+science = int(input("enter your science marks"))
+french = int (input("enter your french marks"))
+sum = math+english+science+french
+print("sum",sum)
+perc = (sum/400)*100
+print("perc",perc)
